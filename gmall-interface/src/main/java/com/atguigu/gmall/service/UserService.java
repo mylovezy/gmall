0cfa9da9af34e7c9ae6d7c74b73edeb24dc015ee
changchen <doublechang123@163.com>
2019-04-10 18:41:25 +0800

    add interface findUserAddressByUserId

--- gmall-interface/src/main/java/com/atguigu/gmall/service/UserService.java
+++ gmall-interface/src/main/java/com/atguigu/gmall/service/UserService.java
@@ -1,5 +1,6 @@
 package com.atguigu.gmall.service;
 
+import com.atguigu.gmall.bean.UserAddress;
 import com.atguigu.gmall.bean.UserInfo;
 
 import java.util.List;
@@ -7,5 +8,9 @@ import java.util.List;
 public interface UserService {
 
     List<UserInfo> findAll();
+
+    //调用根据用户id查询用户地址
+    List<UserAddress> findUserAddressByUserId(String userId);
+
 
 }
