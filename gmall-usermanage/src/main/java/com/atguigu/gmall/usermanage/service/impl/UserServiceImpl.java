57b02b5b5953953665b13e1123248d90c2608c85
changchen <doublechang123@163.com>
2019-04-10 18:46:47 +0800

    update @Service

--- gmall-usermanage/src/main/java/com/atguigu/gmall/usermanage/service/impl/UserServiceImpl.java
+++ gmall-usermanage/src/main/java/com/atguigu/gmall/usermanage/service/impl/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.atguigu.gmall.usermanage.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.bean.UserAddress;
 import com.atguigu.gmall.bean.UserInfo;
 import com.atguigu.gmall.service.UserService;
+import com.atguigu.gmall.usermanage.mapper.UserAddressMapper;
 import com.atguigu.gmall.usermanage.mapper.UserInfoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
@@ -14,8 +16,20 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private UserInfoMapper userInfoMapper;
 
+    @Autowired
+    private UserAddressMapper userAddressMapper;
+
     @Override
     public List<UserInfo> findAll() {
         return userInfoMapper.selectAll();
+    }
+
+    @Override
+    public List<UserAddress> findUserAddressByUserId(String userId) {
+
+        UserAddress userAddress = new UserAddress();
+        userAddress.setUserId(userId);
+        List<UserAddress> userAddressList = userAddressMapper.select(userAddress);
+        return userAddressList;
     }
 }
