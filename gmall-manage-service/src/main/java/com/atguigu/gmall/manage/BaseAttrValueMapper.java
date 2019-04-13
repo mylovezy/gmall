cc55f2da206b317bd20e2eb2992c100a7ad3f671
changchen <doublechang123@163.com>
2019-04-13 23:52:43 +0800

    manage service

---
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/BaseAttrValueMapper.java
@@ -0,0 +1,8 @@
+package com.atguigu.gmall.manage;
+
+import com.atguigu.gmall.bean.BaseAttrValue;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface BaseAttrValueMapper extends Mapper<BaseAttrValue> {
+
+}
