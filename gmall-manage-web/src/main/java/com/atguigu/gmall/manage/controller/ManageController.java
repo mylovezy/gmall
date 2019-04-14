c72275dace7375e0ad2d3562ecf481c03f0e12d2
changchen <doublechang123@163.com>
2019-04-14 16:14:20 +0800

    完善 一级分类 二级分类 三级分类

--- gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/ManageController.java
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/ManageController.java
@@ -1,10 +1,22 @@
 package com.atguigu.gmall.manage.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.gmall.bean.BaseAttrInfo;
+import com.atguigu.gmall.bean.BaseCatalog1;
+import com.atguigu.gmall.bean.BaseCatalog2;
+import com.atguigu.gmall.bean.BaseCatalog3;
+import com.atguigu.gmall.service.ManageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class ManageController {
+
+    @Reference
+    private ManageService manageService;
 
     @RequestMapping("index")
     public String index(){
@@ -15,4 +27,29 @@ public class ManageController {
     public String attrListPage(){
         return  "attrListPage";
     }
+
+    @RequestMapping("getCatalog1")
+    @ResponseBody
+    public List<BaseCatalog1> getBaseCatalog1(){
+        return manageService.getBaseCatalog1();
+    }
+
+    @RequestMapping("getCatalog2")
+    @ResponseBody
+    public List<BaseCatalog2> getBaseCatalog2(String catalog1Id){
+        return manageService.getBaseCatalog2(catalog1Id);
+    }
+
+    @RequestMapping("getCatalog3")
+    @ResponseBody
+    public List<BaseCatalog3> getBaseCatalog3(String catalog2Id){
+        return manageService.getBaseCatalog3(catalog2Id);
+    }
+
+    @RequestMapping("attrInfoList")
+    @ResponseBody
+    public List<BaseAttrInfo> attrInfoList(String catalog3Id){
+        return manageService.getAttrList(catalog3Id);
+    }
+
 }
