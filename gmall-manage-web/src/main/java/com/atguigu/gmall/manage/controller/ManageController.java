411099300514eae06736a4a57577617b924d7bde
changchen <doublechang123@163.com>
2019-04-15 23:52:20 +0800

    新增spu controller

--- gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/ManageController.java
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/ManageController.java
@@ -1,10 +1,7 @@
 package com.atguigu.gmall.manage.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.atguigu.gmall.bean.BaseAttrInfo;
-import com.atguigu.gmall.bean.BaseCatalog1;
-import com.atguigu.gmall.bean.BaseCatalog2;
-import com.atguigu.gmall.bean.BaseCatalog3;
+import com.atguigu.gmall.bean.*;
 import com.atguigu.gmall.service.ManageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +12,7 @@ import java.util.List;
 @Controller
 public class ManageController {
 
+    //容易错
     @Reference
     private ManageService manageService;
 
@@ -58,6 +56,14 @@ public class ManageController {
         manageService.saveAttrInfo(baseAttrInfo);
     }
 
-
+    @RequestMapping("getAttrValueList")
+    @ResponseBody
+    public List<BaseAttrValue> getAttrValueList(String attrId){
+        //平台属性---平台属性值 如果没有平台属性 也就没有平台属性值
+        //从业务理解上 去思考
+        //先通过attrId 查询baseAttrInfo 表建立关系
+        BaseAttrInfo baseAttrInfo = manageService.getAttrInfo(attrId);
+        return baseAttrInfo.getAttrValueList();
+    }
 
 }
